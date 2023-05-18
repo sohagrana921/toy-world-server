13fd58d6b6957f45c44a231cced5ef7d1a9c4026
Sohag Rana <sohagrana921@gmail.com>
2023-05-18 15:26:39 +0600

    search by id is okey

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get("/toys/:id", async (req, res) => {
+      const toy = await toyCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(toy);
+    });
+
     app.get("/toys/:email", async (req, res) => {
       const toys = await toyCollection
         .find({
@@ -59,82 +67,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-// async function run() {
-//     try {
-//         // Connect the client to the server	(optional starting in v4.7)
-//         await client.connect();
-
-//         const serviceCollection = client.db('carDoctor').collection('services');
-//         const bookingCollection = client.db('carDoctor').collection('bookings');
-
-//         app.get('/services', async (req, res) => {
-//             const cursor = serviceCollection.find();
-//             const result = await cursor.toArray();
-//             res.send(result);
-//         })
-
-//         app.get('/services/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) }
-
-//             const options = {
-//                 // Include only the `title` and `imdb` fields in the returned document
-//                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
-//             };
-
-//             const result = await serviceCollection.findOne(query, options);
-//             res.send(result);
-//         })
-
-//         // bookings
-//         app.get('/bookings', async (req, res) => {
-//             console.log(req.query.email);
-//             let query = {};
-//             if (req.query?.email) {
-//                 query = { email: req.query.email }
-//             }
-//             const result = await bookingCollection.find(query).toArray();
-//             res.send(result);
-//         })
-
-//         app.post('/bookings', async (req, res) => {
-//             const booking = req.body;
-//             console.log(booking);
-//             const result = await bookingCollection.insertOne(booking);
-//             res.send(result);
-//         });
-
-//         app.patch('/bookings/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const filter = { _id: new ObjectId(id) };
-//             const updatedBooking = req.body;
-//             console.log(updatedBooking);
-//             const updateDoc = {
-//                 $set: {
-//                     status: updatedBooking.status
-//                 },
-//             };
-//             const result = await bookingCollection.updateOne(filter, updateDoc);
-//             res.send(result);
-//         })
-
-//         app.delete('/bookings/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) }
-//             const result = await bookingCollection.deleteOne(query);
-//             res.send(result);
-//         })
-
-//         // Send a ping to confirm a successful connection
-//         await client.db("admin").command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         // await client.close();
-//     }
-// }
-// run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Toy World Server is Running");
